9fcad9cdaf744fa358bb9f64289fc1acf1684f06
Félicien François <felicien@alvarum.com>
2022-05-30 18:21:29 +0200

    fix missing import

--- src/android/BootReceiver.java
+++ src/android/BootReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.util.Log;
 
 import org.apache.cordova.BuildConfig;
 import org.apache.cordova.stepper.util.API26Wrapper;
