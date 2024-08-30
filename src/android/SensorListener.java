0ca06cf3bd41c5576309dbb97132760aad7f5db9
Félicien <felicien@alvarum.com>
2024-08-30 12:40:31 +0200

    Fix compilation issue

--- src/android/SensorListener.java
+++ src/android/SensorListener.java
@@ -15,7 +15,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH;
+import android.content.pm.ServiceInfo;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
@@ -137,7 +137,7 @@ public class SensorListener extends Service implements SensorEventListener {
 	private void showNotification() {
 		if (getSharedPreferences("pedometer", Context.MODE_PRIVATE).getBoolean("notification", true)) {
 			if (Build.VERSION.SDK_INT >= 34) {
-				startForeground(NOTIFICATION_ID, getNotification(), FOREGROUND_SERVICE_TYPE_HEALTH);
+				startForeground(NOTIFICATION_ID, getNotification(), ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH);
 			} else if (Build.VERSION.SDK_INT >= 26) {
 				startForeground(NOTIFICATION_ID, getNotification());
 			} else {
